fd555ac4a7d6461dbe44ae134670fa866c056abc
Islam <sagov-2001@mail.ru>
2021-11-07 11:52:55 +0300

    Перенес код

--- js/script.js
+++ js/script.js
@@ -3,6 +3,23 @@ const database = fetch('../database.json').then(data => data.json());
 const loc = window.location.pathname;
 
 
+function minusNumber(btn) {
+    const count = btn.nextElementSibling
+    let countNum = count.innerHTML
+    if (countNum <= 1) {
+        countNum = 1
+    } else {
+        count.textContent = --countNum
+    }
+}
+
+
+function plusNumber(btn) {
+    const count = btn.previousElementSibling
+    let countNum = count.innerHTML
+    count.textContent = ++countNum
+}
+
 function checkForEmptyCart() {
     const html = `
     <div class="es-absence">
