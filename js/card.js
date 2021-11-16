a714dde24f880abf7a690844e9417e3db0323dc7
Islam <sagov-2001@mail.ru>
2021-11-16 17:19:46 +0300

    Переработал код слайдера карточки товара

--- js/card.js
+++ js/card.js
@@ -101,13 +101,21 @@ class Card {
         
     }
 
-    slider(btn, btnName) {
-
+    rightSlide(btn) {
         const imagesContainer = btn.parentNode.parentNode.querySelector('.es-card__slider-list');
-        this.numOfPiexels == 66 ? this.numOfPiexels = 0 : this.numOfPiexels += 33;
+        let pixels = parseInt(imagesContainer.dataset.pixels);
+        pixels == 66 ? pixels = 0 : pixels += 33;
+        imagesContainer.dataset.pixels = pixels
+        imagesContainer.style.transform = `translate(-${pixels}%, 0px)`
 
+    }
 
-        imagesContainer.style.transform = `translate(-${this.numOfPiexels}%, 0px)`
+    leftSlide(btn) {
+        const imagesContainer = btn.parentNode.parentNode.querySelector('.es-card__slider-list');
+        let pixels = parseInt(imagesContainer.dataset.pixels);
+        pixels == 0 ? pixels = 66 : pixels -= 33;
+        imagesContainer.dataset.pixels = pixels
+        imagesContainer.style.transform = `translate(-${pixels}%, 0px)`
     }
 
     render() {
@@ -119,19 +127,19 @@ class Card {
                 <li class="es-card" data-id="${id}">
                     <div class="es-card__slider">
                         <div class="es-card__slider-buttons">
-                            <button class="es-btn-slider es-card__slider-btn-left" id="btn-left" onclick="card.slider(this)">
+                            <button class="es-btn-slider es-card__slider-btn-left" id="btn-left" onclick="card.leftSlide(this)">
                                 <svg class="es-btn-slider__svg-left" width="10" height="6" viewBox="0 0 10 6" fill="none" xmlns="http://www.w3.org/2000/svg">
                                     <path d="M5.3902 0.162908L9.84025 4.70779C9.94325 4.8129 10 4.95323 10 5.10285C10 5.25247 9.94325 5.39279 9.84025 5.49791L9.51261 5.83261C9.29911 6.0504 8.95212 6.0504 8.73895 5.83261L5.00207 2.01616L1.26105 5.83684C1.15804 5.94196 1.02072 6 0.874301 6C0.727717 6 0.590401 5.94196 0.487313 5.83684L0.159754 5.50214C0.056747 5.39694 -2.29018e-07 5.2567 -2.27233e-07 5.10708C-2.25449e-07 4.95746 0.056747 4.81714 0.159754 4.71202L4.61386 0.162908C4.7172 0.0575419 4.85516 -0.000330684 5.00183 1.44263e-06C5.14906 -0.00033068 5.28695 0.0575419 5.3902 0.162908Z"/>
                                 </svg>
                             </button>
-                            <button class="es-btn-slider es-card__slider-btn-right" id="btn-right" onclick="card.slider(this)">
+                            <button class="es-btn-slider es-card__slider-btn-right" id="btn-right" onclick="card.rightSlide(this)">
                                 <svg class="es-btn-slider__svg-right es-rotate" width="10" height="6" viewBox="0 0 10 6" fill="none" xmlns="http://www.w3.org/2000/svg">
                                     <path d="M5.3902 0.162908L9.84025 4.70779C9.94325 4.8129 10 4.95323 10 5.10285C10 5.25247 9.94325 5.39279 9.84025 5.49791L9.51261 5.83261C9.29911 6.0504 8.95212 6.0504 8.73895 5.83261L5.00207 2.01616L1.26105 5.83684C1.15804 5.94196 1.02072 6 0.874301 6C0.727717 6 0.590401 5.94196 0.487313 5.83684L0.159754 5.50214C0.056747 5.39694 -2.29018e-07 5.2567 -2.27233e-07 5.10708C-2.25449e-07 4.95746 0.056747 4.81714 0.159754 4.71202L4.61386 0.162908C4.7172 0.0575419 4.85516 -0.000330684 5.00183 1.44263e-06C5.14906 -0.00033068 5.28695 0.0575419 5.3902 0.162908Z"/>
                                 </svg>
                             </button>
                         </div>
                         <div class="es-card__slider-container">
-                            <div class="es-card__slider-list">
+                            <div class="es-card__slider-list" data-pixels="0">
                                 <div class="es-card__slider-slide">
                                     <img src="${images[0]}" alt="${title}">
                                 </div>
