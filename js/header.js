12df1f703db0728b816d24bea02f9922fe8dd631
Islam <sagov-2001@mail.ru>
2023-03-21 15:41:53 +0300

    Правка

--- js/header.js
+++ js/header.js
@@ -85,7 +85,7 @@ class Header {
                                 
                             <div class="es-input-search es-header__search">
                                 <input class="es-input-search__input" type="search" placeholder="Поиск">
-                                <a class="es-input-search__btn" href="/search-page.html" onclick="saveSearchValue(this)" onauxclick="saveSearchValue(this)">
+                                <a class="es-input-search__btn" href="search-page.html" onclick="saveSearchValue(this)" onauxclick="saveSearchValue(this)">
                                     <svg width="20" height="20" viewBox="0 0 35 35" fill="none" xmlns="http://www.w3.org/2000/svg">
                                         <path  class="es-input-search__btn-icon" d="M34.4678 31.8795L25.8397 23.2158C28.0582 20.6697 29.2737 17.4662 29.2737 14.1312C29.2737 6.3394 22.7075 0 14.6368 0C6.56621 0 0 6.3394 0 14.1312C0 21.9231 6.56621 28.2625 14.6368 28.2625C17.6667 28.2625 20.5539 27.3802 23.0225 25.7053L31.7161 34.4348C32.0795 34.7991 32.5682 35 33.092 35C33.5877 35 34.058 34.8175 34.415 34.4857C35.1736 33.781 35.1978 32.6124 34.4678 31.8795ZM14.6368 3.68641C20.6023 3.68641 25.4554 8.37184 25.4554 14.1312C25.4554 19.8906 20.6023 24.5761 14.6368 24.5761C8.67137 24.5761 3.81831 19.8906 3.81831 14.1312C3.81831 8.37184 8.67137 3.68641 14.6368 3.68641Z"/>
                                     </svg>
@@ -99,7 +99,7 @@ class Header {
                             </button>
 
                             <div class="es-header__user-buttons">
-                                <a class="es-header__chosen-btn" href="/account.html">
+                                <a class="es-header__chosen-btn" href="account.html">
                                     <span class="es-header__quantity-products" id="chosen-quantity-products">3</span>
                                     <img src="images/chosen.svg" alt="Chosen">
                                 </a>
@@ -133,7 +133,7 @@ class Header {
                                             </div>
                                             <div class="es-header__cart-footer">
                                                 <p class="es-amount">Итого: <span class="es-amount-sum">0 ₽</span></p>
-                                                <a class="es-btn es-btn--size-all-width es-btn--style-inherit-orange es-margin-top  es-btn--link" href="/cart.html" id="cart-btn">Перейти в коризну</a>
+                                                <a class="es-btn es-btn--size-all-width es-btn--style-inherit-orange es-margin-top  es-btn--link" href="cart.html" id="cart-btn">Перейти в коризну</a>
                                             </div>
                                         </div>
                                     </div>
@@ -151,7 +151,7 @@ class Header {
                                         <div class="es-header__menu-bg-2" >
                                             <div class="es-hide es-input-search es-header__search">
                                                 <input class="es-input-search__input" type="search" placeholder="Поиск">
-                                                <a class="es-input-search__btn" href="/search-page.html" onclick="saveSearchValue(this)" onauxclick="saveSearchValue(this)">
+                                                <a class="es-input-search__btn" href="search-page.html" onclick="saveSearchValue(this)" onauxclick="saveSearchValue(this)">
                                                     <svg width="20" height="20" viewBox="0 0 35 35" fill="none" xmlns="http://www.w3.org/2000/svg">
                                                         <path  class="es-input-search__btn-icon" d="M34.4678 31.8795L25.8397 23.2158C28.0582 20.6697 29.2737 17.4662 29.2737 14.1312C29.2737 6.3394 22.7075 0 14.6368 0C6.56621 0 0 6.3394 0 14.1312C0 21.9231 6.56621 28.2625 14.6368 28.2625C17.6667 28.2625 20.5539 27.3802 23.0225 25.7053L31.7161 34.4348C32.0795 34.7991 32.5682 35 33.092 35C33.5877 35 34.058 34.8175 34.415 34.4857C35.1736 33.781 35.1978 32.6124 34.4678 31.8795ZM14.6368 3.68641C20.6023 3.68641 25.4554 8.37184 25.4554 14.1312C25.4554 19.8906 20.6023 24.5761 14.6368 24.5761C8.67137 24.5761 3.81831 19.8906 3.81831 14.1312C3.81831 8.37184 8.67137 3.68641 14.6368 3.68641Z"/>
                                                     </svg>
